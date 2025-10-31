07617c5dee31549b1309c543a9e7b8a5dc6b88b0
Samuel Maia Candido <Samuel54164586@edu.df.senac.br>
2025-10-31 15:23:15 -0300

    commit1402

--- docs/script.js
+++ docs/script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (userData && userData.usuario === usuario && userData.senha === senha) {
             alert('Login realizado com sucesso!');
-            window.location.href = './html/dashfacil.html';
+            window.location.href = 'html/dashfacil.html';
         } else {
             alert('Usuário ou senha incorretos. Por favor, tente novamente.');
         }
